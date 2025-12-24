485b9a14529b3dcfefc90bc535319e28629b477d
Mrafi <mrafiramadhan24@gmail.com>
2025-12-25 03:15:28 +0700

    downgrade nativewind

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import "../global.css";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
